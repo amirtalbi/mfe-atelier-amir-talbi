102ef716a23bef85a6752632dfd8bb74983e9dd7
amirtalbi <amirotalbi94@gmail.com>
2025-03-03 12:53:23 +0100

    Fixing last errors and renaming header package.json

--- shell/src/index.js
+++ shell/src/index.js
@@ -1,18 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-import('./bootstrap')
-  .then(({ start }) => {
-    start();
-  })
-  .catch(err => {
-    console.error('Error loading the application:', err);
-  });
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import("./bootstrap").catch((err) => {
+  console.error("Error loading the application:", err);
+});
