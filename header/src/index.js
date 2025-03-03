76f0aca1abfe97254c94d5eb55b0731a6e806618
amirtalbi <amirotalbi94@gmail.com>
2025-03-03 12:46:22 +0100

    Adding error handling

--- header/src/index.js
+++ header/src/index.js
@@ -8,7 +8,13 @@ const render = () => {
 };
 
 if (process.env.NODE_ENV === "development") {
-  bootstrap().then(render);
+  import("./bootstrap")
+    .then(({ bootstrap }) => {
+      bootstrap().then(render);
+    })
+    .catch(err => {
+      console.error("Error bootstrapping header application:", err);
+    });
 } else {
   render();
 }
